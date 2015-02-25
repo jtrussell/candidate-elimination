dce8a508a3fe538fb35d5a9edece1c44e1dc056a
jtrussell <jus.russell@gmail.com>
2015-02-25 15:01:46 -0500

    Working hypothesis + examples

--- app/components/hypothesis/service.hypo-hypothesis.js
+++ app/components/hypothesis/service.hypo-hypothesis.js
@@ -18,9 +18,7 @@ angular.module('hypo.hypothesis.service', [])
      * @return {boolean} Whether or not this hypothesis is consistent with `ex`
      */
     Hypothesis.prototype.isConsistentWith = function(ex) {
-      var containsEx = this.lowerLeftX <= ex.x && ex.x <= this.topRightX &&
-          this.lowerLeftY <= ex.y && ex.y <= this.topRightY;
-      return containsEx === ex.isPositive;
+      return contains(this, ex) === ex.isPositive;
     };
 
     /**
@@ -34,7 +32,10 @@ angular.module('hypo.hypothesis.service', [])
      * @return {boolean} True if we're more general than `hypo`
      */
     Hypothesis.prototype.isMoreGeneralThan = function(hypo) {
-      return true;
+      return this.lowerLeftX <= hypo.lowerLeftX &&
+        this.lowerLeftY <= hypo.lowerLeftY &&
+        this.topRightX >= hypo.topRightX &&
+        this.topRightY >= hypo.topRightY;
     };
 
     /**
@@ -48,7 +49,10 @@ angular.module('hypo.hypothesis.service', [])
      * @return {boolean} True if we're more general than `hypo`
      */
     Hypothesis.prototype.isMoreSpecificThan = function(hypo) {
-      return true;
+      return this.lowerLeftX >= hypo.lowerLeftX &&
+        this.lowerLeftY >= hypo.lowerLeftY &&
+        this.topRightX <= hypo.topRightX &&
+        this.topRightY <= hypo.topRightY;
     };
 
     /**
@@ -66,23 +70,100 @@ angular.module('hypo.hypothesis.service', [])
     /**
      * Get the minimal specializations of `hypo` to accomodate `ex` if any
      *
+     * Will only specialize for negative examples inconsistent with this
+     * hypothesis.
+     *
      * @param {Hypothesis} hypo The hypothesis
      * @param {Example} ex The example
      * @return {Array<Hypothesis>} The array of specialized hypotheses
      */
     exports.specializeFor = function(hypo, ex) {
-      return [];
+      if(ex.isPositive || !contains(hypo, ex)) {
+        return [clone(hypo)];
+      }
+
+      // Ex is negative and in a range that this hypothesis claims as positive.
+      // We can make up to four new minimal specializations of this
+      // hypothesis...
+      var specializations = [];
+      
+      // Northern specialization...
+      // When the example is not on the northern border of the hypothesis
+      if(hypo.topRightY !== ex.y) {
+        specializations.push(exports(
+          hypo.lowerLeftX,
+          ex.y + 1,
+          hypo.topRightX,
+          hypo.topRightY
+        ));
+      }
+
+      // Eastern specialization...
+      if(hypo.topRightX !== ex.x) {
+        specializations.push(exports(
+          ex.x + 1,
+          hypo.lowerLeftY,
+          hypo.topRightX,
+          hypo.topRightY
+        ));
+      }
+
+      // Southern specialization...
+      if(hypo.lowerLeftY !== ex.y) {
+        specializations.push(exports(
+          hypo.lowerLeftX,
+          hypo.lowerLeftY,
+          hypo.topRightX,
+          ex.y - 1
+        ));
+      }
+
+      // Western specialization...
+      if(hypo.lowerLeftY !== ex.y) {
+        specializations.push(exports(
+          hypo.lowerLeftX,
+          hypo.lowerLeftY,
+          ex.x - 1,
+          hypo.topRightY
+        ));
+      }
+
+      return specializations;
     };
 
     /**
      * Get the minimal generalizations of `hypo` to accomodate `ex` if any
+     *
+     * Will only generalize for positive examples.
      *
      * @param {Hypothesis} hypo The hypothesis
      * @param {Example} ex The example
      * @return {Array<Hypothesis>} The array of generalized hypotheses
      */
     exports.generalizeFor = function(hypo, ex) {
-      return [];
+      return ex.isPositive ? [exports(
+        Math.min(hypo.lowerLeftX, ex.x),
+        Math.min(hypo.lowerLeftY, ex.y),
+        Math.max(hypo.topRightX, ex.x),
+        Math.max(hypo.topRightY, ex.y)
+      )] : [clone(hypo)];
+    };
+
+    // Does the given hypothesis contain the example (does not check for
+    // consistency).
+    var contains = function(hypo, ex) {
+      return hypo.lowerLeftX <= ex.x && ex.x <= hypo.topRightX &&
+        hypo.lowerLeftY <= ex.y && ex.y <= hypo.topRightY;
+    };
+
+    // Clone a hypothesis
+    var clone = function(hypo) {
+      return exports(
+        hypo.lowerLeftX,
+        hypo.lowerLeftY,
+        hypo.topRightX,
+        hypo.topRightY
+      );
     };
 
     return exports;
