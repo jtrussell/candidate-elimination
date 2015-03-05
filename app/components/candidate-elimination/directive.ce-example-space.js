e7a801dc964787bb207456e8d874b7bfa5475717
Justin <jus.russell@gmail.com>
2015-03-05 11:31:59 -0500

    Playing around with some charting...

--- app/components/candidate-elimination/directive.ce-example-space.js
+++ app/components/candidate-elimination/directive.ce-example-space.js
@@ -1,11 +1,72 @@
 
-angular.module('ce.example-space.directive', ['d3'])
-  .directive('ceExampleSpace', function() {
+angular.module('ce.example-space.directive', ['d3', 'uni'])
+  .directive('ceExampleSpace', function(d3, UNI_MIN, UNI_MAX) {
     'use strict';
     return {
       restrict: 'AE',
-      template: '[chart coming soon]',
-      link: function() {}
+      template: '<div></div>',
+      link: function(scope, element, attrs) {
+        element = element.children().eq(0);
+
+        var el = element[0]
+          , height = 200
+          , width = 200;
+
+        var margin = {
+          top: 20,
+          right: 0,
+          left: 20,
+          bottom: 20
+        };
+
+        element.css('height', (height + margin.top + margin.bottom) + 'px');
+        element.css('width', (width + margin.right + margin.left) + 'px');
+
+        var x = d3.scale.linear()
+          .domain([UNI_MIN, UNI_MAX])
+          .range([0, width]);
+
+        var y = d3.scale.linear()
+          .domain([UNI_MIN, UNI_MAX])
+          .range([height, 0]);
+
+        var xAxis = d3.svg.axis()
+          .scale(x)
+          .ticks(10)
+          .tickSize(height)
+          .orient('bottom');
+
+        var yAxis = d3.svg.axis()
+          .scale(y)
+          .ticks(10)
+          .tickSize(width)
+          .orient('right');
+
+        var svg = d3.select(element[0])
+          .append('svg')
+            .attr('height', height + margin.top + margin.bottom)
+          .append('g');
+
+        var gx = svg.append('g')
+          .attr('class', 'x axis')
+          .call(xAxis);
+
+        gx.selectAll('g').filter(function(d) { {return d;} })
+          .classed('minor', true);
+
+        //gx.selectAll('text')
+
+        var gy = svg.append('g')
+          .attr('class', 'y axis')
+          .call(yAxis);
+
+        gy.selectAll('g').filter(function(d) { {return d;} })
+          .classed('minor', true);
+
+        gy.selectAll('text')
+          .attr('x', 4)
+          .attr('dy', -4);
+      }
     };
   });
 
